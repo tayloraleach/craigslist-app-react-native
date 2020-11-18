bcffd705bf704efccddd3e65d6840093401843b6
Taylor <taylor@euphorica.com>
2020-11-17 18:20:00 -0800

    moved inline styles to obj

--- screens/Home.js
+++ screens/Home.js
@@ -21,104 +21,73 @@ function HomeScreen({navigation}) {
   const [ownerType, setOwnerType] = React.useState(OWNER_TYPE.ALL);
 
   return (
-    <View
-      style={{
-        flex: 1,
-        margin: 20,
-      }}>
-      <View
-        style={{
-          borderColor: colors.purple,
-          borderWidth: 1,
-          borderRadius: 2,
-          marginBottom: 20,
-        }}>
+    <View style={styles.root}>
+      <View style={styles.locationPickerContainer}>
         <Picker
           dropdownIconColor={colors.purple}
           selectedValue={location}
-          style={{height: 40, width: '100%'}}
+          style={styles.locationPicker}
           onValueChange={itemValue => setLocation(itemValue)}>
           <Picker.Item label="Vancouver" value="vancouver" />
           <Picker.Item label="Comox Valley" value="comoxvalley" />
         </Picker>
       </View>
 
-      <View style={{marginBottom: 20}}>
+      <View style={styles.buttonGroupContainer}>
         <ButtonGroup
           items={[OWNER_TYPE.ALL, OWNER_TYPE.OWNER, OWNER_TYPE.DEALER]}
           selectedItem={OWNER_TYPE.ALL}
           onChange={value => setOwnerType(value)}
         />
       </View>
-      <View style={{flexDirection: 'row', alignItems: 'center'}}>
-        <CheckBox
-          tintColors={{
-            true: colors.purple,
-            false: colors.grey,
-          }}
-          disabled={false}
-          value={hasImages}
-          onValueChange={newValue => setHasImages(newValue)}
-        />
-        <Text>Has Image</Text>
+      <View style={styles.togglesContainer}>
+        <View style={styles.checkBoxContainer}>
+          <CheckBox
+            tintColors={{
+              true: colors.purple,
+              false: colors.grey,
+            }}
+            disabled={false}
+            value={hasImages}
+            onValueChange={newValue => setHasImages(newValue)}
+          />
+          <Text>Has Image</Text>
+        </View>
+        <View style={styles.checkBoxContainer}>
+          <CheckBox
+            tintColors={{
+              true: colors.purple,
+              false: colors.grey,
+            }}
+            disabled={false}
+            value={postedToday}
+            onValueChange={newValue => setPostedToday(newValue)}
+          />
+          <Text>Posted Today</Text>
+        </View>
+        <View style={styles.checkBoxContainer}>
+          <CheckBox
+            tintColors={{
+              true: colors.purple,
+              false: colors.grey,
+            }}
+            disabled={false}
+            value={searchTitlesOnly}
+            onValueChange={newValue => setSearchTitlesOnly(newValue)}
+          />
+          <Text>Titles Only</Text>
+        </View>
       </View>
-      <View style={{flexDirection: 'row', alignItems: 'center'}}>
-        <CheckBox
-          tintColors={{
-            true: colors.purple,
-            false: colors.grey,
-          }}
-          disabled={false}
-          value={postedToday}
-          onValueChange={newValue => setPostedToday(newValue)}
-        />
-        <Text>Posted Today</Text>
-      </View>
-      <View style={{flexDirection: 'row', alignItems: 'center'}}>
-        <CheckBox
-          tintColors={{
-            true: colors.purple,
-            false: colors.grey,
-          }}
-          disabled={false}
-          value={searchTitlesOnly}
-          onValueChange={newValue => setSearchTitlesOnly(newValue)}
-        />
-        <Text>Search Titles Only</Text>
-      </View>
-      <View
-        style={{
-          marginTop: 20,
-          justifyContent: 'center',
-          alignItems: 'center',
-          flexDirection: 'row',
-        }}>
+      <View style={styles.searchContainer}>
         <TextInput
           placeholder="Search..."
           onChangeText={term => setSearchTerm(term)}
           value={searchTerm}
-          style={{
-            height: 40,
-            borderColor: colors.grey,
-            paddingLeft: 15,
-            borderWidth: 1,
-            borderRightWidth: 0,
-            borderTopLeftRadius: 2,
-            borderBottomLeftRadius: 2,
-            flex: 1,
-          }}
+          style={styles.searchBar}
         />
         <TouchableOpacity
           activeOpacity={0.75}
-          style={{
-            width: 100,
-            height: 40,
-            borderBottomRightRadius: 2,
-            borderTopRightRadius: 2,
-            backgroundColor: colors.purple,
-            alignItems: 'center',
-            justifyContent: 'center',
-          }}
+          style={styles.searchButton}
           onPress={() =>
             navigation.navigate('SearchResults', {
               searchTerm,
@@ -129,11 +98,56 @@ function HomeScreen({navigation}) {
               ownerType,
             })
           }>
-          <Text style={{color: 'white'}}>Search</Text>
+          <Text style={styles.searchButtonText}>Search</Text>
         </TouchableOpacity>
       </View>
     </View>
   );
 }
+
+const styles = {
+  root: {
+    flex: 1,
+    margin: 20,
+  },
+  locationPickerContainer: {
+    borderColor: colors.grey,
+    borderWidth: 1,
+    borderRadius: 2,
+    marginBottom: 10,
+  },
+  locationPicker: {height: 40, width: '100%'},
+  buttonGroupContainer: {marginBottom: 10},
+  togglesContainer: {flexDirection: 'row', flexWrap: 'wrap'},
+  checkBoxContainer: {flexDirection: 'row', alignItems: 'center'},
+  searchContainer: {
+    marginTop: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
+    flexDirection: 'row',
+  },
+  searchBar: {
+    height: 40,
+    borderColor: colors.grey,
+    paddingLeft: 15,
+    borderWidth: 1,
+    borderRightWidth: 0,
+    borderTopLeftRadius: 2,
+    borderBottomLeftRadius: 2,
+    flex: 1,
+  },
+  searchButton: {
+    width: 100,
+    height: 40,
+    borderBottomRightRadius: 2,
+    borderTopRightRadius: 2,
+    backgroundColor: colors.purple,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  searchButtonText: {
+    color: 'white',
+  },
+};
 
 export default HomeScreen;
