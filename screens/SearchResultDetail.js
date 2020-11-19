2bbfe514817ee65eeae68d5cc7d7ee965fa56323
Taylor <taylor@euphorica.com>
2020-11-19 08:00:51 -0800

    styled single listing images and html content with render from cl body

--- screens/SearchResultDetail.js
+++ screens/SearchResultDetail.js
@@ -1,40 +1,70 @@
 import * as React from 'react';
-import {Image, Text, View, ScrollView} from 'react-native';
+import {Image, Text, View, ScrollView, useWindowDimensions} from 'react-native';
 import colors from '../lib/colors';
 import fetcher from '../lib/fetcher';
+import HTML from 'react-native-render-html';
+import Loader from '../components/Loader';
 
 function SearchResultDetail({route}) {
+  // TODO: This might break for expired listings
   const {title, images, price, hood, url: listingURL} = route.params;
 
   const [listingData, setListingData] = React.useState(null);
 
   React.useEffect(() => {
     (async () => {
-      let url = `http://192.168.1.73:5000/listing?url=${listingURL}`;
-      const data = await fetcher(encodeURI(url));
-      console.log(data);
+      if (!listingData) {
+        let url = `http://192.168.1.73:5000/listing?url=${listingURL}`;
+        try {
+          const data = await fetcher(encodeURI(url));
+          setListingData(data);
+        } catch (e) {
+          console.log('Maybe it was delted or expired?', url, e);
+        }
+      }
     })();
-  }, [listingURL]);
+  }, [listingData, listingURL]);
+
+  const contentWidth = useWindowDimensions().width;
 
   return (
     <View style={styles.root}>
-      <Text style={styles.hood}>{hood}</Text>
+      {hood ? <Text style={styles.hood}>{hood}</Text> : null}
       <Text style={styles.title}>{title}</Text>
       <Text style={styles.price}>{price}</Text>
-      <ScrollView contentContainerStyle={styles.scrollView}>
-        {images.map(uri => {
-          return (
-            <Image
-              key={uri}
-              resizeMode={'cover'}
-              style={styles.image}
-              source={{
-                uri,
-              }}
-            />
-          );
-        })}
-      </ScrollView>
+      {listingData ? (
+        <ScrollView style={{flex: 1}}>
+          <HTML
+            classesStyles={{
+              'print-qrcode-container': {
+                display: 'none',
+              },
+              root: {
+                color: 'black',
+              },
+            }}
+            html={`<div class="root">${listingData.data.contentBody}</div>`}
+            contentWidth={contentWidth}
+          />
+          {console.log(listingData.data.contentBody)}
+          <ScrollView contentContainerStyle={styles.scrollView}>
+            {images.map(uri => {
+              return (
+                <Image
+                  key={uri}
+                  resizeMode={'cover'}
+                  style={{...styles.image, width: contentWidth / 2 - 12.5}}
+                  source={{
+                    uri,
+                  }}
+                />
+              );
+            })}
+          </ScrollView>
+        </ScrollView>
+      ) : (
+        <Loader />
+      )}
     </View>
   );
 }
@@ -45,15 +75,15 @@ const styles = {
     flex: 1,
   },
   scrollView: {
-    justifyContent: 'center',
+    justifyContent: 'space-between',
     flexDirection: 'row',
     flexWrap: 'wrap',
     alignItems: 'flex-start',
+    marginTop: 20,
   },
   image: {
-    width: '47%',
     height: 200,
-    margin: 5,
+    marginTop: 5,
   },
   price: {fontSize: 20, color: colors.purple, marginBottom: 20},
   title: {fontSize: 28, fontWeight: 'bold', marginBottom: 10},
